3105a146d89e0886143056b978aefb75aaa8ad19
Flavio Santiago <fsantiago@live.com>
2016-08-29 17:55:55 -0300

    Atualizacao na geracao de best and worst inputs

--- Initializer.cpp
+++ Initializer.cpp
@@ -16,39 +16,45 @@ int main()
 
 	switch (sortType)
 	{
-	//Insertion
+	//InsertionSort
 	case 0:
 		for (int i = nrItems; i > 0; i--)
 			list.push_back(i);
 		InsertionSort(&list);
 		break;
+	//SelectionSort - (Tanto faz a arrumacao pois sempre olha todos os itens em busca do maior para colocar na ultima posicao)
 	case 1:
 		break;
+	//BubbleSort
 	case 2:
+		for (int i = nrItems; i > 0; i--)
+			list.push_back(i);
+		BubbleSort(&list);
 		break;
+	//MergeSort - (Tanto faz pois divide o array e vai comparando um a u, até preencher um terceiro array)
 	case 3:
 		break;
+	//QuickSort
 	case 4:
 		break;
+	//TreeSort
 	case 5:
 		break;
+	//HeapSort
 	case 6:
 		break;
+	//BucketSort
 	case 7:
 		break;
+	//CountingSort
 	case 8:
 		break;
+	//RadixSort
 	case 9:
 		break;
 	default:
 		break;
 	}
 
-	Tree x = Tree(nrNodes);
-	Tree y = isomorphic ? x.GenerateIsomorphic() : x.GenerateNonIsomorphic();
-
-	if (IsIsomorphic(&x, &y) != isomorphic)
-		return 1;
-	else
-		return 0;
+	return 1;
 }
